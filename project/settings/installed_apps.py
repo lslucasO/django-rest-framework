bc27612a68a83272244a4d665660f0a856e64eb5
lslucaso <lslemos59@gmail.com>
2025-03-18 20:27:31 -0300

    initial commit

---
+++ project/settings/installed_apps.py
@@ -0,0 +1,15 @@
+INSTALLED_APPS = [
+    # Django
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Django rest framework
+    'rest_framework',
+    # Estes são nossos apps
+    'recipes',
+    'authors',
+    'tag',
+]
